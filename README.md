6b6ebee745c18141424adb6f3af1fa4e9c17a089
Guillaume Barbas <guillaumebarbas@hotmail.fr>
2025-03-14 15:39:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# itinov-bank
+ 
---
+++ src/main/java/com/example/itinovbank/ItinovBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.itinovbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItinovBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItinovBankApplication.class, args);
+    }
+
+}
