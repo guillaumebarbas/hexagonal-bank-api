0fa070ef850ebcd7be3f4106dd4d568d1f3aaa84
Guillaume Barbas <guillaumebarbas@hotmail.fr>
2025-03-14 16:00:15 +0100

    feat(creation_entities): Debut du projet , creation entities infrastructure

---
+++ src/main/java/infrastructure/entity/CompteDatabase.java
@@ -0,0 +1,24 @@
+package infrastructure.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "compte")
+public class CompteDatabase {
+
+    @Id
+    private Long id;
+
+}
